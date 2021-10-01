2b93fe9055f407829da6c5e35d6cd641dc32a10d
apurva3455 <59577787+apurva3455@users.noreply.github.com>
2021-10-02 03:47:05 +0530

    Add files via upload

---
+++ Python/modinverse.py
@@ -0,0 +1,17 @@
+def modInverse(a, m):
+    m0 = m
+    y = 0
+    x = 1
+    if m == 1:
+        return 0
+    while a > 1:
+        q = a // m
+        t = m
+        m = a % m
+        a = t
+        t = y
+        y = x - q * y
+        x = t
+    if x < 0:
+        x = x + m0
+    return x
