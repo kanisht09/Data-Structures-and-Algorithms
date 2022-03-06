dd93f9ef65df6a5e6fdf68a1ee287e76e85e3dac
Kanisht Agarwal <55939035+kanisht09@users.noreply.github.com>
2022-03-06 09:27:43 +0530

    Added Floyd Warshall (all pairs shortest path)

--- C++/Algorithms/Graph Algorithms/Floyd_Warshall.cpp
+++ C++/Algorithms/Graph Algorithms/Floyd_Warshall.cpp
@@ -23,11 +23,6 @@ void printv(vector<ll>&a)
 
 int main()
 {
-
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
    
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
@@ -64,8 +59,7 @@ int main()
             }
         }
     }
-
-     for(ll i=1;i<=n;i++)
+    for(ll i=1;i<=n;i++)
     {
         for(ll j=1;j<=n;j++)
         {
@@ -76,11 +70,3 @@ int main()
   
     return 0;
 }
-
-
-
-
-
-
-
-
