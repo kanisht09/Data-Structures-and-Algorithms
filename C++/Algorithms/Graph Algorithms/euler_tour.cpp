cc477a4872664c7ec394487b8f5cbdf05943f5d1
Kanisht Agarwal <55939035+kanisht09@users.noreply.github.com>
2023-03-21 19:06:15 +0530

    Update euler_tour.cpp

--- C++/Algorithms/Graph Algorithms/euler_tour.cpp
+++ C++/Algorithms/Graph Algorithms/euler_tour.cpp
@@ -1,133 +1,136 @@
 #include <bits/stdc++.h>
-#define mod 1000000007
-#define ull unsigned ll
-#define ll long long int
+#define ll long long
 #define ld long double
 #define pb push_back
-#define mp make_pair
-#define mt make_tuple
 #define ff first
 #define ss second
-#define pi acos(-1)
-#define nui 100
-#define INF 100000000000
-
+#define vll vector<ll>
 #define F(i,a,n) for(ll i=a;i<n;i++)
-#define B(i,a,n) for(ll i=n-1;i>=a;i--)
-#define tc int t;cin>>t;while(t--)
-#define tcf int t;cin>>t;for(int w=1;w<=t;w++)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 
 
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
 
 using namespace std;
+
+/*
+
+
+
+*/
+
+const int N = 100001;
+const int mod = 1e9 + 7;
 
 
 ll binpow(ll a, ll b)
 {
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-        {
-            res *= a;
-        }
-        a *= a;
-        b >>= 1;
-    }
-    return res;
+	ll res = 1;
+	while (b > 0)
+	{
+		if (b & 1)
+		{
+			res *= a;
+		}
+		a *= a;
+		b >>= 1;
+	}
+	return res;
 }
 
 ll fermat_inv(ll y) {return binpow(y, mod - 2);}
 
 
-// Euler Tree Technique
 
-// -> representation of a tree in the form of an array
+vector<int>adj[N];
+vector<int>st(N);
+vector<int>et(N);
+vector<int>ft(2 * N);
 
-// flat tree array will be twice the size of number of nodes
+int _time = 0;
+void dfs(int src, int par = -1) {
 
-vector<ll>adj[200005];
-vector<ll>FT(400005);
-vector<ll>S(200005);
-vector<ll>T(200005);
-
-ll timer=1;
-
-void dfs(ll node,ll par)
-{
-    FT[timer]=node;
-    S[node]=timer;
-    timer++;
-    for(auto u:adj[node])
-    {
-        if(u!=par)
-        {
-            dfs(u,node);
-        }
-    }
-    FT[timer]=node;
-    T[node]=timer;
-    timer++;
+	st[src] = ++_time;
+	ft[_time] = src;
+	for (auto i : adj[src]) {
+		if (i != par) {
+			dfs(i, src);
+		}
+	}
+	et[src] = ++_time;
+	ft[_time] = src;
 }
 
 
+int main() {
 
-int main()
-{
+	int tc;
+	tc = 1;
+	// cin >> tc; // take test cases
+	while (tc--) {
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+		int n, x, y;
+		cin >> n;
 
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+		/*
 
- 
-    int t;
-    t=1;
-    // cin>>t;
-    while(t--)
-    {
-        ll n,m;
-        cin>>n;
-        m=n-1;
-        ll x,y;
-        for(ll i=1;i<=n;i++)
-        {
-            adj[i].clear();
-            FT[i]=0;
-            S[i]=0;
-            T[i]=0;
-        }
-        for(ll i=0;i<m;i++)
-        {
-            cin>>x>>y;
-            adj[x].pb(y);
-            adj[y].pb(x);
-        }
-        timer=1;
-        dfs(1,-1);
-        for(ll i=1;i<=2*n;i++)
-        {
-            cout<<FT[i]<<" ";
-        }
-        cout<<"\n";
-        for(ll i=1;i<=n;i++)
-        {
-            cout<<S[i]<<" ";
-        }
-        cout<<"\n";
-        for(ll i=1;i<=n;i++)
-        {
-            cout<<T[i]<<" ";
-        }
-        cout<<"\n";
+			4
+			1 2
+			1 3
+			3 4
 
-    }
-    
+		*/
 
 
+		for (int i = 0; i < n - 1; i++) {
+			cin >> x >> y;
+			adj[x].pb(y);
+			adj[y].pb(x);
 
-    return 0;
+		}
+
+		_time = 0;
+		dfs(1, -1);
+		for (int i = 1; i <= n; i++) {
+			cout << st[i] << ' ';
+		}
+		cout << '\n';
+		for (int i = 1; i <= n; i++) {
+			cout << et[i] << ' ';
+		}
+		cout << '\n';
+		for (int i = 1; i <= 2 * n; i++) {
+			cout << ft[i] << ' ';
+		}
+		cout << '\n';
+
+		/*
+
+			If we want to calculate the number of nodes in the subtree of node x
+
+			we just need to know the starting time of node x and its ending time
+
+			number of nodes in the subtree of node x = (et[x]-st[x]-1)/2
+
+		*/
+
+		int src = 3;
+		cout << (et[src] - st[src] - 1) / 2 << '\n';
+
+
+
+
+	}
+
+
+	return 0;
 
 }
